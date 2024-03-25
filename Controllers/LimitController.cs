49f0b6ac49ab59e4368cd98acd86532b62c36029
vadzimblr <vadim.poznyak.wrk@gmail.com>
2024-03-25 20:18:42 +0300

    Add Limit controller

---
+++ Controllers/LimitController.cs
@@ -0,0 +1,34 @@
+﻿using Contracts.ServiceContracts;
+using Controllers.ActionFilters;
+using Controllers.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dto.UpdateResourcesDto;
+
+namespace Controllers;
+[ApiController]
+[Authorize]
+[Route("api/limits")]
+public class LimitController:ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public LimitController(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetLimits()
+    {
+        var result = await _serviceManager.Limit.GetLimit(HttpContext.GetUserId(), trackChanges: false);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilter))]
+    public async Task<IActionResult> UpdateLimits([FromBody] ManipulationLimitDto? limitDto)
+    {
+        await _serviceManager.Limit.CreateLimit(limitDto);
+        return StatusCode(201);
+    }
+}
